d6eded2301d0f57ee841b90a9463d2a327982b60
Olrando <204797682@qq.com>
2023-07-05 13:35:49 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box">hello</div>
+</template>
+
+<script setup lang="ts">
+const a = '我爱你'
+</script>
+<style scoped>
+.box {
+  height: 100px;
+  width: 100px;
+  background-color: red;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+createApp(App).mount('#app')
+
+const fn = () => {
+  console.log('我爱你祖国')
+}
+
+fn()
